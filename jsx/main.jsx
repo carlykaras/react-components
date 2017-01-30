b00872119f044163505b99dcdc4edb9465e10da1
Carly Karas <carly.karas@gmail.com>
2017-01-29 19:32:54 -0500

    Added div

--- jsx/main.jsx
+++ jsx/main.jsx
@@ -5,6 +5,6 @@ import Card from './Card.jsx';
 ReactDOM.render(
     <div>
         <Card img="https://placeholdit.imgix.net/~text?txtsize=40&txt=250%C3%97150&w=250&h=150"/>,
-    </div>
+    </div>,
     document.getElementById("container")
 );
