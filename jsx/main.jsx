a5cec5096a5bc198113afa96df20eaaf5769e6ef
Carly Karas <carly.karas@gmail.com>
2017-01-29 19:30:03 -0500

    Put components into div

--- jsx/main.jsx
+++ jsx/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import Card from './Card.jsx';
 
 ReactDOM.render(
-    <Card img="https://placeholdit.imgix.net/~text?txtsize=40&txt=250%C3%97150&w=250&h=150"/>,
+    <div>
+        <Card img="https://placeholdit.imgix.net/~text?txtsize=40&txt=250%C3%97150&w=250&h=150"/>,
+    </div>
     document.getElementById("container")
 );
